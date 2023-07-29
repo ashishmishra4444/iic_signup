2fd38d844a16abfb39bfba029463bab5795ad62b
Ashish <ashishanil334@gmail.com>
2023-07-29 11:59:42 +0530

    initial commit

---
+++ src/sign.js
@@ -0,0 +1,54 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const mongoose=require("mongoose");
+const app=express();
+const path=require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+const port=process.env.PORT || 3000;
+const staticPath=path.join(__dirname,"../public");
+app.use(cors());
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+mongoose.connect("mongodb+srv://ashishmishra:AHAAMISHRANP@cluster0.zxpdeie.mongodb.net/user?retryWrites=true&w=majority")
+const schema={
+    name:String,
+    address:String,
+    gender:String,
+    country:String,
+    age:Number,
+    email:String,
+    password:String
+  }
+  const userDetails=mongoose.model("userDetails",schema);
+  app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname + "../public/index.html"));
+  });
+  app.post("/",function(req,res){
+    let newUser=new userDetails({
+      name:req.body.name,
+      address:req.body.address,
+      gender:req.body.gender,
+      country:req.body.country,
+      age:req.body.age,
+      email:req.body.email,
+      password:req.body.password
+    })
+    newUser.save();
+    res.redirect("/");
+  });
+  app.listen(port,function(){
+    console.log(`Server started at port ${port}`);
+  });
